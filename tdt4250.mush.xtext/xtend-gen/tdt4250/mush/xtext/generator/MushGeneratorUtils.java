9b64f42b0db5e04e28e19e462420550ee586ea3a
André Storhaug <andr3.storhaug@gmail.com>
2020-12-12 18:21:25 +0100

    Add _trace to gitignore

---
+++ tdt4250.mush.xtext/xtend-gen/tdt4250/mush/xtext/generator/MushGeneratorUtils.java
@@ -0,0 +1,21 @@
+package tdt4250.mush.xtext.generator;
+
+@SuppressWarnings("all")
+public class MushGeneratorUtils {
+  public static String replaceLast(final String find, final String replace, final String string) {
+    int lastIndex = string.lastIndexOf(find);
+    if ((lastIndex == (-1))) {
+      return string;
+    }
+    String beginString = string.substring(0, lastIndex);
+    int _length = find.length();
+    int _plus = (lastIndex + _length);
+    String endString = string.substring(_plus);
+    return ((beginString + replace) + endString);
+  }
+  
+  public static String escape(final String s) {
+    return s.replace("\\", "\\\\").replace("\t", "\\t").replace("\b", "\\b").replace("\n", "\\n").replace("\r", 
+      "\\r").replace("\f", "\\f").replace("\'", "\\\'").replace("\"", "\\\"");
+  }
+}
