f9f5fa723f437e11ee0f97a08d38f844a9653f54
Annabelle <annabelle.almaas@gmail.com>
2020-12-13 20:28:33 +0100

    example

---
+++ tdt4250.mush.examples/src-gen/ReverseNumber2.java
@@ -0,0 +1,19 @@
+public class ReverseNumber2 {
+    public static void run(int nr) {
+        int lastDigit = nr%10;
+        int reverse = 0;
+        reverse = ((reverse*10)+lastDigit);
+        nr = nr/10;
+        while (nr>0) {
+            lastDigit = nr%10;
+            reverse = (reverse*10)+lastDigit;
+            nr = nr/10;
+        }
+        System.out.println(reverse);
+    }
+    
+    public static void main(final String[] args) {
+        int ARG0 = Integer.parseInt(args[0]);
+        run(ARG0);
+    }
+}
